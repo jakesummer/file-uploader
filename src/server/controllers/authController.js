21e76451e6be1d678719b769d5bd463d36c5ee0d
jakesummer <jakersummer@gmail.com>
2026-07-15 16:11:25 -0400

    feat: add server side form validation to sign up page

--- src/server/controllers/authController.js
+++ src/server/controllers/authController.js
@@ -1,3 +1,4 @@
+import { body, matchedData, validationResult } from "express-validator";
 import { createNewUser } from "../db/queries/userQueries.js";
 import { hashPassword } from "../lib/passwordUtils.js";
 
@@ -5,12 +6,46 @@ export function signUpGet(req, res) {
   res.render("sign-up");
 }
 
-export async function signUpPost(req, res, next) {
-  try {
-    const hashedPassword = await hashPassword(req.body.password);
-    await createNewUser(req.body.username, hashedPassword);
-    res.send("success!!");
-  } catch (err) {
-    next(err);
-  }
-}
+const lengthErr = (min, max) => `must be between ${min} and ${max} characters!`;
+const validateUser = [
+  body("username")
+    .trim()
+    .isLength({ min: 2, max: 16 })
+    .withMessage(`Username ${lengthErr(2, 16)}`)
+    .matches(/^[a-zA-Z0-9_.]+$/)
+    .withMessage(
+      "Username can only contain letters, numbers, underscores, and periods!",
+    ),
+  body("password")
+    .trim()
+    .isLength({ min: 8, max: 16 })
+    .withMessage(`Password ${lengthErr(8, 16)}`)
+    .matches(/^[a-zA-Z0-9_.[\]{}()!@#$%^&*+\-=\\/|:;'",<>?`~]+$/)
+    .withMessage(
+      "Password can only contain letters, numbers, and special characters!",
+    ),
+];
+
+export const signUpPost = [
+  validateUser,
+  async (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      const errorsArr = errors.array();
+      return res.status(400).render("sign-up", {
+        username: req.body.username,
+        usernameErrorMsg: errorsArr.find((e) => e.path === "username")?.msg,
+        passwordErrorMsg: errorsArr.find((e) => e.path === "password")?.msg,
+      });
+    }
+
+    try {
+      const { username, password } = matchedData(req);
+      const hashedPassword = await hashPassword(password);
+      await createNewUser(username, hashedPassword);
+      res.send("success!!");
+    } catch (err) {
+      next(err);
+    }
+  },
+];
