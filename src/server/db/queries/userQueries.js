4329ecddaa9d895e4ef1a2b40b59442c52f784c8
jakesummer <jakersummer@gmail.com>
2026-07-15 14:44:23 -0400

    style: change userQueries.js to use inline exports

--- src/server/db/queries/userQueries.js
+++ src/server/db/queries/userQueries.js
@@ -1,15 +1,13 @@
 import prisma from "../prisma.js";
 
-async function getUserByUsername(username) {
+export async function getUserByUsername(username) {
   return prisma.user.findUnique({
     where: { username },
   });
 }
 
-async function getUserById(id) {
+export async function getUserById(id) {
   return prisma.user.findUnique({
     where: { id },
   });
 }
-
-export { getUserByUsername, getUserById };
