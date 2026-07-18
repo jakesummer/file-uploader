23c2d5a3739cf18129824a447f48108f1c6c6c64
jakesummer <jakersummer@gmail.com>
2026-07-17 22:32:18 -0400

    feat: add new folder and file dialogs with forms

--- src/client/entries/dashboard.js
+++ src/client/entries/dashboard.js
@@ -1,2 +1,7 @@
 import "../styles/global.css";
 import "../styles/dashboard.css";
+
+const newFolderDialog = document.getElementById("new-folder-dialog");
+const newFolderForm = document.getElementById("new-folder-form");
+
+newFolderDialog.addEventListener("wa-after-hide", () => newFolderForm.reset());
