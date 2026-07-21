52eaa638253c149dfd2f2ae7450b3d45692a9a96
jakesummer <jakersummer@gmail.com>
2026-07-21 11:36:42 -0400

    fix: await createNewFolder function call

--- src/server/controllers/itemController.js
+++ src/server/controllers/itemController.js
@@ -3,6 +3,6 @@ import { createNewFolder } from "../db/queries/itemQueries.js";
 export async function createFolderPost(req, res) {
   const folderName = req.body.name;
   const userId = req.user.id;
-  createNewFolder(folderName, userId);
+  await createNewFolder(folderName, userId);
   res.redirect("/dashboard");
 }
