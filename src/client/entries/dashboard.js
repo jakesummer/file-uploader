cd4da1cbc8034da7613ee47e2349bf0c62b7260c
jakesummer <jakersummer@gmail.com>
2026-07-23 15:18:40 -0400

    feat: add client side validation to file upload

--- src/client/entries/dashboard.js
+++ src/client/entries/dashboard.js
@@ -2,11 +2,16 @@ import "../styles/global.css";
 import "../styles//dashboard/dashboard.css";
 import "../styles//dashboard/item.css";
 import "../styles//dashboard/tree.css";
+import "../styles//dashboard/dialogs.css";
 
 const newFolderDialog = document.getElementById("new-folder-dialog");
 const newFolderForm = document.getElementById("new-folder-form");
 const newFileDialog = document.getElementById("new-file-dialog");
 const newFileForm = document.getElementById("new-file-form");
+const fileInput = document.getElementById("new-file-input");
+const fileHint = document.getElementById("new-file-hint");
+
+const MAX_FILE_SIZE_MB = 50;
 
 newFolderDialog.addEventListener("wa-after-hide", () => newFolderForm.reset());
 newFileDialog.addEventListener("wa-after-hide", () => newFileForm.reset());
@@ -26,4 +31,21 @@ document.addEventListener("click", (e) => {
   document.getElementById("dialog-file-created").textContent = created;
   document.getElementById("delete-file-form").action = `/item/delete/${id}`;
   document.getElementById("dialog-download-btn").href = `/item/download/${id}`;
+});
+
+fileInput.addEventListener("change", () => {
+  const files = fileInput.files;
+  if (files.length > 0) {
+    const fileSize = files[0].size;
+    const maxSize = MAX_FILE_SIZE_MB * 1024 * 1024;
+
+    if (fileSize > maxSize) {
+      const errMsg = `File exceeds the ${MAX_FILE_SIZE_MB}MB limit!`;
+      fileInput.setCustomValidity(errMsg);
+      fileHint.textContent = errMsg;
+    } else {
+      fileInput.setCustomValidity("");
+      fileHint.textContent = "";
+    }
+  }
 });
