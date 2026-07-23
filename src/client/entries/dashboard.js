c286046b4f83c427e47dcadda7fc2c7c4641ad48
jakesummer <jakersummer@gmail.com>
2026-07-22 20:51:35 -0400

    feat: clear file upload form when dialog is closed

--- src/client/entries/dashboard.js
+++ src/client/entries/dashboard.js
@@ -5,8 +5,11 @@ import "../styles//dashboard/tree.css";
 
 const newFolderDialog = document.getElementById("new-folder-dialog");
 const newFolderForm = document.getElementById("new-folder-form");
+const newFileDialog = document.getElementById("new-file-dialog");
+const newFileForm = document.getElementById("new-file-form");
 
 newFolderDialog.addEventListener("wa-after-hide", () => newFolderForm.reset());
+newFileDialog.addEventListener("wa-after-hide", () => newFileForm.reset());
 
 customElements.whenDefined("wa-tree-item").then(() => {
   document.querySelectorAll(".expanded").forEach((el) => (el.expanded = true));
