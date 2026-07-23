8047ada1f64f42d1543f31b6968231dc857a6949
jakesummer <jakersummer@gmail.com>
2026-07-22 21:57:25 -0400

    feat: format file size

--- src/server/utils/ejsHelpers.js
+++ src/server/utils/ejsHelpers.js
@@ -5,3 +5,12 @@ export function formatDate(date) {
     year: "numeric",
   }).format(date);
 }
+
+// adapted from: https://gist.github.com/zentala/1e6f72438796d74531803cc3833c039c
+export function formatBytes(bytes) {
+  if (bytes == 0) return "0 Bytes";
+  const k = 1024;
+  const sizes = ["Bytes", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"];
+  const i = Math.floor(Math.log(bytes) / Math.log(k));
+  return parseFloat((bytes / Math.pow(k, i)).toFixed(2)) + " " + sizes[i];
+}
